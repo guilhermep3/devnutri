8565fdf07cacce7a610f60fc351eaaaa7404cf7c
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2025-06-04 09:35:28 -0300

    add hero

---
+++ src/components/layout/hero.tsx
@@ -0,0 +1,21 @@
+import { buttonStyle, containerStyle } from "@/utlis/styles"
+import { Button } from "../ui/button"
+import { HeroImage } from "./hero-image"
+
+export const Hero = () => {
+
+  return (
+    <section id="hero" className="relative w-full h-screen bg-[url('/hero/hero.jpg')] bg-cover bg-left sm:bg-center">
+      <div className={containerStyle + ' flex justify-center items-center flex-col md:flex-row gap-4 z-10'}>
+        <div className="flex-1 flex flex-col gap-4">
+          <h1 className="text-4xl sm:text-5xl lg:text-7xl font-extrabold sourGummy">Coma Bem, Viva Melhor.</h1>
+          <p className="text-lg sm:text-xl font-semibold">Cuide de você e viva melhor, compre nossas marmitas fitness e agende uma consulta com nossos nutricionistas.</p>
+          <Button className={buttonStyle + ' rounded-tl-4xl rounded-br-4xl px-8 py-6 text-lg font-semibold'}>Comprar</Button>
+        </div>
+        <div className="sm:flex-1 flex flex-col justify-center items-center">
+          <HeroImage/>
+        </div>
+      </div>
+    </section>
+  )
+}
