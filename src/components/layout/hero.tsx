c77d45b383dcb02af420dbb67b920e03e54d04bf
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2025-06-04 15:02:42 -0300

    add about page

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -6,9 +6,9 @@ export const Hero = () => {
 
   return (
     <section id="hero" className="relative w-full h-screen bg-[url('/hero/hero.jpg')] bg-cover bg-left sm:bg-center">
-      <div className={containerStyle + ' flex justify-center items-center flex-col md:flex-row gap-4 z-10'}>
-        <div className="flex-1 flex flex-col gap-4">
-          <h1 className="text-4xl sm:text-5xl lg:text-7xl font-extrabold sourGummy">Coma Bem, Viva Melhor.</h1>
+      <div className={containerStyle + ' flex justify-center items-center flex-col md:flex-row md:gap-4 z-10 pb-10 sm:pb-20'}>
+        <div className="flex-1 flex flex-col gap-4 justify-end items-center md:items-start text-center md:text-start">
+          <h1 className="text-4xl sm:text-5xl lg:text-7xl font-extrabold">Coma Bem, Viva Melhor.</h1>
           <p className="text-lg sm:text-xl font-semibold">Cuide de você e viva melhor, compre nossas marmitas fitness e agende uma consulta com nossos nutricionistas.</p>
           <Button className={buttonStyle + ' rounded-tl-4xl rounded-br-4xl px-8 py-6 text-lg font-semibold'}>Comprar</Button>
         </div>
